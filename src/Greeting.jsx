c4f624abae69b3eefb768580964c17dd81cc51ab
perugi <dominik.perusko@gmail.com>
2025-03-05 19:54:50 +0100

    Playing around with keys

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,15 @@
 function Greeting() {
-  return <p>I will end you.</p>
+  const animals = ["cat", "dog", "bird"];
+  return (
+    <div>
+      <p>I will end you.</p>
+      <ul>
+        {animals.map((animal) => (
+          <li>{animal}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Greeting;
