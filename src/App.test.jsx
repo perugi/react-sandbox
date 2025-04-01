5fbf7516384fc37f028041644c6b5b822c468bd3
perugi <dominik.perusko@gmail.com>
2025-04-01 13:25:42 +0200

    Add react testing libraries

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,6 @@
 import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
 describe("something truthy and falsy", () => {
   it("true to be true", () => {
@@ -7,5 +9,13 @@ describe("something truthy and falsy", () => {
 
   it("false to be false", () => {
     expect(false).toBe(false);
+  });
+});
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App />);
+
+    screen.debug();
   });
 });
