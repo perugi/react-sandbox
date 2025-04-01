e5eb7e9c756197a2e774ccaf7d481f6a4f76a255
perugi <dominik.perusko@gmail.com>
2025-04-01 22:34:17 +0200

    First simple render test

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import FilterableProductTable from "./FilterableProductTable";
 import PRODUCTS from "./data";
 
 function App() {
-  return (
-    <>
-      <FilterableProductTable products={PRODUCTS} />
-    </>
-  );
+  return <h1>Our First Test</h1>;
 }
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,20 +2,9 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("something truthy and falsy", () => {
-  it("true to be true", () => {
-    expect(true).toBe(true);
-  });
-
-  it("false to be false", () => {
-    expect(false).toBe(false);
-  });
-});
-
-describe("App", () => {
-  it("renders headline", () => {
+describe("App component", () => {
+  it("renders correct heading", () => {
     render(<App />);
-
-    screen.debug();
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
