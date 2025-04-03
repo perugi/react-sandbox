ffb655d78ee725778a505997cbc6e2878a855ba4
perugi <dominik.perusko@gmail.com>
2025-04-03 10:46:16 +0200

    Add basic test example from Odin React Testing Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import FilterableProductTable from "./FilterableProductTable";
-import PRODUCTS from "./data";
+import { useState } from "react";
 
 function App() {
-  return <h1>Our First Test</h1>;
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const handleClick = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>Click me</button>
+      <h1>{heading}</h1>
+    </>
+  );
 }
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,23 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
+import { use } from "react";
 
 describe("App component", () => {
-  it("renders correct heading", () => {
+  it("renders default heading", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after user click", async () => {
+    const user = userEvent.setup();
+
     render(<App />);
-    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    const button = screen.getByRole("button", { name: /click me/i });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
