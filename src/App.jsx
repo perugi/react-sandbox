e964aa32eb1844a8494e37f37e14525261e2c31f
perugi <dominik.perusko@gmail.com>
2025-09-25 22:51:18 +0200

    Add .jsx example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import styles from "./App.module.css";
 
+const person = {
+  name: "Gregorio Y. Zara",
+  theme: {
+    backgroundColor: "black",
+    color: "red",
+  },
+};
+
 function App() {
   return (
-    <>
-      <p className={styles.title}>Hello React</p>
-    </>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img src="https://imgur.com/7vQD0fPs.jpg" alt="" />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronatics lectures</li>
+        <li>Work on the alcohol-fueled engine</li>
+      </ul>
+    </div>
   );
 }
 
