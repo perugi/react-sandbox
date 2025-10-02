f0497b283bd792c867b341fc3ac6b6f5a7ac32ed
perugi <dominik.perusko@gmail.com>
2025-10-02 22:23:56 +0200

    Add erroneous state example

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,23 @@
 import styles from "./App.module.css";
 import { v4 as uuid } from "uuid";
-
-function ListItem({ animal }) {
-  console.log({ animal });
-  return <li>{animal}</li>;
-}
-
-function List({ animals }) {
-  if (!animals) {
-    return <div>Loading...</div>;
-  }
-
-  if (animals.length === 0) {
-    return <div>There are no animals in the list!</div>;
-  }
-
-  console.log({ animals });
-
-  return (
-    <ul>
-      {animals.map((animal) => {
-        return <ListItem key={animal} animal={animal} />;
-      })}
-    </ul>
-  );
-}
-
-const todos = [
-  { task: "mow the yard", id: uuid() },
-  { task: "work on TOP", id: uuid() },
-  { task: "feed the cat", id: uuid() },
-];
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.task}</li>
-      ))}
-    </ul>
+    <div>
+      <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber(number + 1);
+          setNumber(number + 1);
+          setNumber(number + 1);
+        }}
+      >
+        +3
+      </button>
+    </div>
   );
 }
 
