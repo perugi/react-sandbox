409e0b78264734f67caa7f730c19215e81297026
perugi <dominik.perusko@gmail.com>
2025-10-02 22:38:56 +0200

    Add state example (async function)

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,44 @@ import styles from "./App.module.css";
 import { useState } from "react";
 
 function App() {
-  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("typing");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("sending");
+    await sendMessage(text);
+    setStatus("sent");
+  }
+
+  const isSending = status === "sending";
+  const isSent = status === "sent";
+
+  if (isSent) {
+    return <h1>Thanks!</h1>;
+  }
 
   return (
-    <div>
-      <h1>{number}</h1>
-      <button
-        onClick={() => {
-          setNumber((prevNumber) => prevNumber + 1);
-          setNumber((prevNumber) => prevNumber + 1);
-          setNumber((prevNumber) => prevNumber + 1);
-        }}
-      >
-        +3
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay?</p>
+      <textarea
+        disabled={isSending}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      <button disabled={isSending} type="submit">
+        Send
       </button>
-    </div>
+      {isSending && <p>Sending...</p>}
+    </form>
   );
 }
 
 export default App;
+
+function sendMessage(text) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+}
