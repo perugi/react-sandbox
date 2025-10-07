30e074a86b11811729195b2f8b869bc282256602
perugi <dominik.perusko@gmail.com>
2025-10-07 22:53:38 +0200

    Add computed state example

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,31 @@
 import styles from "./App.module.css";
-import { useState } from "react";
-
-function Panel({ title, isActive, makeActive, children }) {
-  return (
-    <section className="panel">
-      <h3>{title}</h3>
-      {isActive ? (
-        <p>{children}</p>
-      ) : (
-        <button onClick={() => makeActive()}>Show</button>
-      )}
-    </section>
-  );
-}
+import { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [activeIdx, setActiveIdx] = useState(0);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const isValid = name && email.includes("@");
 
   return (
-    <>
-      <h2>Almaty, Kazakhstan</h2>
-      <Panel
-        title="About"
-        isActive={activeIdx === 0}
-        makeActive={() => setActiveIdx(0)}
-      >
-        With a population of about 2 million, Almaty is Kazakhstan's largest
-        city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel
-        title="Etymology"
-        isActive={activeIdx === 1}
-        makeActive={() => setActiveIdx(1)}
-      >
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
-        "apple" and is often translated as "full of apples". In fact, the region
-        surrounding Almaty is thought to be the ancestral home of the apple, and
-        the wild <i lang="la">Malus sieversii</i> is considered a likely
-        candidate for the ancestor of the modern domestic apple.
-      </Panel>
-    </>
+    <div>
+      <label for="name">Name:</label>
+      <input
+        id="name"
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <label for="email">Email:</label>
+      <input
+        id="email"
+        type="text"
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <button type="submit" disabled={!isValid}>
+        Submit
+      </button>
+    </div>
   );
 }
 
